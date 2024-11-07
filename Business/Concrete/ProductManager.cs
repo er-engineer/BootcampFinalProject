b167b78f3e1b7ea2c25f4a8ad2dee22379cca94b
Eren Memiş <69437351+er-engineer@users.noreply.github.com>
2024-11-07 21:54:42 +0300

    End of episode #7

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            // Business Codes
+            return _productDal.GetAll(); 
+        }
+    }
+}
